d1d1178aba0a41b56af3afd2de8839efc633e730
Jedrzej Nowacki <contact@jnowacki.pl>
2019-09-29 12:56:18 +0200

    Album model

--- src/main/java/pl/jnowacki/AlbumServlet.java
+++ src/main/java/pl/jnowacki/AlbumServlet.java
@@ -1,5 +1,36 @@
 package pl.jnowacki;
 
-public class AlbumServlet {
-    //tytuł, autora, rok wydania
+import java.io.Serializable;
+
+public class AlbumServlet implements Serializable {
+    private String title;
+    private String author;
+    private int year;
+
+    public AlbumServlet() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
